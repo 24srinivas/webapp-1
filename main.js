7abfc84aa73817277f5041d8317eaeb1bb2b86e2
1krishna <krishrockstar1@gmail.com>
2018-07-19 12:14:19 +0530

    final

--- main.js
+++ main.js
@@ -1,21 +1,22 @@
-function getfile(file,callback){
-  var xhr=new XMLHttpRequest();
-  xhr.overrideMimeType("application/json");
-  xhr.open("GET",file,true);
-  xhr.onreadystatechange=function(){
-    if(xhr.readyState === 4 && xhr.status =="200"){
-      callback(xhr.responseText);
-    }
-};
-xhr.send(null);
-}
-getfile("data.json",function(text){
-  var data=JSON.parse(text);
-  console.log(data);
-  details(data.basic);
-  detail(data.obj);
-
-})
+// function getfile(file,callback){
+//   var xhr=new XMLHttpRequest();
+//   xhr.overrideMimeType("application/json");
+//   xhr.open("GET",file,true);
+//   xhr.onreadystatechange=function(){
+//     if(xhr.readyState === 4 && xhr.status =="200"){
+//       callback(xhr.responseText);
+//     }
+// };
+// xhr.send(null);
+// }
+// getfile("data.json",function(text){
+//   var data=JSON.parse(text);
+//   console.log(data);
+//   details(data.basic);
+//   detail(data.obj);
+//   education(data.education);
+//   skill(data.skills);
+// })
 var child = document.querySelector(".childone");
 function details(det){
   var img =document.createElement("img");
@@ -63,3 +64,73 @@ function detail(det){
 
 
 }
+
+
+
+function education(edu){
+
+  var education=document.createElement("h3");
+  education.textContent="Education Qualification";
+  child1.appendChild(education);
+
+  var line=document.createElement("hr");
+  child1.appendChild(line);
+
+  for(i=0;i<edu.length;i++){
+
+    var deg=document.createElement("h3");
+    deg.textContent=edu[i].degree;
+    child1.appendChild(deg);
+
+    var edu1=document.createElement("li");
+    var edu2=document.createElement("li");
+    edu2.textContent=edu[i].institute;
+    edu1.textContent=edu[i].data;
+    child1.appendChild(edu2);
+    child1.appendChild(edu1);
+
+  }
+}
+  function skill(tech){
+
+      var title=document.createElement("h3");
+      title.textContent="Technical skills";
+      child1.appendChild(title);
+
+      var line=document.createElement("hr");
+      child1.appendChild(line);
+
+      var skilldata=document.createElement("table");
+      skilldata.border="1";
+      child1.appendChild(skilldata);
+
+      tabledata="";
+      for(i=0;i<tech.length;i++)
+      {
+         tabledata+="<tr> <td>"+tech[i].data+"<td>"+tech[i].lang;
+      }
+      skilldata.innerHTML=tabledata;
+  }
+
+function loadjson(file){
+  return new Promise((resolve,reject)=>{
+  return fetch(file).then(response=>{
+    if(response.ok){
+      resolve(response.json());
+    }
+    else {
+      reject(new Error("error"));
+    }
+
+  })
+  })
+}
+
+var newfile = loadjson("data.json");
+  newfile.then(data=>{
+    details(data.basic);
+    detail(data.obj);
+    education(data.education);
+    skill(data.skills);
+
+  })
